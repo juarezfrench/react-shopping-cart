6ae187eabce5a85a76bdfea3d1878c467728fe11
juarezfrench <markedfrench@gmail.com>
2019-10-07 17:49:32 -0500

    Added addItem functionality, Product and Cart contexts done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
 	const [products] = useState(data);
@@ -13,29 +15,28 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
-		setCart([...cart, item])
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{ cart }}>
+					<Navigation cart={cart} />
+					<Route exact path="/" component={Products} />
+					{/* Routes */}
+					{/* <Route
 				exact
 				path="/"
 				render={() => (
 					<Products
 						products={products}
 						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					/> */}
+					)} />
+					<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,5 @@
 import {createContext} from 'react';
 
  
-export  const ProductContext = createContext(); 
- 
+export const ProductContext = createContext();
   
-
